b70a20110ee8395af40abf8fac7ae52f0130a9df
Olivier Turcotte <olivier.turcotte.18@ulaval.ca>
2018-04-10 13:21:30 -0400

    Ajout d'un algo

--- simul.R
+++ simul.R
@@ -5,14 +5,23 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 setwd("./Données Denis")
 
 temp <- read.csv2("Poids15-30_2.csv",dec = ",",header = F)
-V <- matrix(0,nrow = n,ncol = 30)
+poids <- matrix(0,nrow = n,ncol = 30)
 
-for (j in seq(2)){
+for (j in seq(n)){
     c <- numeric(0)
     for (i in names(temp)){
         c <- c(c,temp[[i]][j])
     }
-    V[j,] <- c
+    poids[j,] <- c
 }
-V[1,]
 
+ecart <- read.csv2("Ecart.csv",dec = ",", header =F)
+corr <- read.csv2("Corr.csv",dec = ",",header = F)
+
+a <- numeric(0)
+for(i in seq(30)){
+    for( j in seq(30)){
+        a <- c(a,poids[1,i]*poids[1,j]*ecart[i]*ecart[j]*corr[max(i,j),min(i,j)])
+    }
+}
+sum(unlist(a))
